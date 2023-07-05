ad3aedde620345e080725a85e2f792e8f646bbd2
Osinachi-Uro <osinachi.ibiam@gmail.com>
2023-07-05 23:02:06 +0000

    added Helm installation script

--- Jenkins/jenkins-install.sh
+++ Jenkins/jenkins-install.sh
@@ -61,6 +61,16 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
+
+# install helm
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+apt-get update
+apt-get install helm
+
+
 # install ansible
 apt update
 #apt install software-properties-common -y
