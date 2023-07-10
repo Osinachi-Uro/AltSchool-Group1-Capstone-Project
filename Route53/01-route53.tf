3459f03192cbcde95d6840e509793ce2f6179b4e
Osinachi-Uro <osinachi.ibiam@gmail.com>
2023-07-10 21:56:02 +0000

    Added route53 folder

---
+++ Route53/01-route53.tf
@@ -0,0 +1,12 @@
+# Create hostedzone
+resource "aws_route53_zone" "hosted_zone" {
+  name = var.domain_name
+}
+
+# Attached the subdomain and shows the ALB
+resource "aws_route53_record" "site_domain" {
+  zone_id = aws_route53_zone.hosted_zone.zone_id
+  name    = "schoolapp.${var.domain_name}"
+  type    = "A"
+
+}
---
+++ Route53/02-variables.tf
@@ -0,0 +1,5 @@
+variable "domain_name" {
+  description = "Domain name"
+  default     = "adeoluja.live"
+  type        = string
+}
