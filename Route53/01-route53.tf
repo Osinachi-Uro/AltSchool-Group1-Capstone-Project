2b9695f1eaa1489bc98fbff98137d94987efbb5b
Osinachi-Uro <osinachi.ibiam@gmail.com>
2023-07-11 23:20:02 +0000

    modified deployment file

--- Route53/01-route53.tf
+++ Route53/01-route53.tf
@@ -4,7 +4,7 @@ resource "aws_route53_zone" "hosted_zone" {
 }
 
 # Attached the subdomain and shows the ALB
-resource "aws_route53_record" "site_domain" {
+resource "aws_route53_record" "schoolapp" {
   zone_id = aws_route53_zone.hosted_zone.zone_id
   name    = "schoolapp.${var.domain_name}"
   type    = "A"
