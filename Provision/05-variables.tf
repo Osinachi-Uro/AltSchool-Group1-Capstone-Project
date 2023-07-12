78188be059e6c3cbd8fc6468de0b235096a04f55
Osinachi Ibiam-Uro <83463641+Osinachi-Uro@users.noreply.github.com>
2023-07-13 00:15:48 +0100

    Update 05-variables.tf

--- Provision/05-variables.tf
+++ Provision/05-variables.tf
@@ -7,11 +7,11 @@ variable "region" {
   default = "eu-west-1"
 }
 
-vpc_cidr_block = "10.0.0.0/16"
+variable vpc_cidr_block = "10.0.0.0/16"
 
-private_subnet_cidr_blocks = ["10.0.2.0/24", "10.0.3.0/24"]
+variable private_subnet_cidr_blocks = ["10.0.2.0/24", "10.0.3.0/24"]
 
-public_subnet_cidr_blocks = ["10.0.5.0/24", "10.0.6.0/24"]
+variable public_subnet_cidr_blocks = ["10.0.5.0/24", "10.0.6.0/24"]
 
 
 #variable "vpc_cidr_block" {}
