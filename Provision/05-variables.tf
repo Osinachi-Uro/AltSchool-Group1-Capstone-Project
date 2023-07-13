f16c3fb39bf874b9e4560e51f6436f5ba939ad99
Adeoluwa Adeyemo <108673398+AdeoluJa@users.noreply.github.com>
2023-07-13 18:52:44 +0100

    Update 05-variables.tf

--- Provision/05-variables.tf
+++ Provision/05-variables.tf
@@ -7,15 +7,24 @@ variable "region" {
   default = "eu-west-1"
 }
 
-variable "vpc_cidr_block" {}
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
 
-variable "private_subnet_cidr_blocks" {}
+variable "private_subnet_cidr_blocks" {
+  type    = list(any)
+  default = ["10.0.2.0/24", "10.0.3.0/24"]
+}
 
-variable "public_subnet_cidr_blocks" {}
+variable "public_subnet_cidr_blocks" {
+  type    = list(any)
+  default = ["10.0.5.0/24", "10.0.6.0/24"]
+}
 
-vpc_cidr_block = "10.0.0.0/16"
-private_subnet_cidr_blocks = ["10.0.2.0/24", "10.0.3.0/24"]
-public_subnet_cidr_blocks = ["10.0.5.0/24", "10.0.6.0/24"]
+# vpc_cidr_block = "10.0.0.0/16"
+# private_subnet_cidr_blocks = ["10.0.2.0/24", "10.0.3.0/24"]
+# public_subnet_cidr_blocks = ["10.0.5.0/24", "10.0.6.0/24"]
 
 # variable "kube_config" {
 #   type    = string
