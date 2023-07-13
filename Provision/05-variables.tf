cab86dc4c60d788d4f32a0e64eda2ebd59a4ff22
Osinachi Ibiam-Uro <83463641+Osinachi-Uro@users.noreply.github.com>
2023-07-13 18:45:39 +0100

    Update 05-variables.tf

--- Provision/05-variables.tf
+++ Provision/05-variables.tf
@@ -7,12 +7,17 @@ variable "region" {
   default = "eu-west-1"
 }
 
-variable "vpc_cidr_block" {}
+variable "vpc_cidr_block" {
+  vpc_cidr_block = "10.0.0.0/16"
+}
 
-variable "private_subnet_cidr_blocks" {}
+variable "private_subnet_cidr_blocks" {
+  private_subnet_cidr_blocks = ["10.0.2.0/24", "10.0.3.0/24"]
+}
 
-variable "public_subnet_cidr_blocks" {}
-
+variable "public_subnet_cidr_blocks" {
+  public_subnet_cidr_blocks = ["10.0.5.0/24", "10.0.6.0/24"]
+}
 
 # variable "kube_config" {
 #   type    = string
